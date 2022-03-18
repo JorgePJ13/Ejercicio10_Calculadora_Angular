c0905f68c6c9045a17fb6e58c31997d7f208838d
Jorge Pando Jimeno <jorge.pando@babelgroup.com>
2022-03-18 10:58:23 +0100

    Implementacion Calculadora en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculado_Angular';
+  cabecera = 'Calculadora de Jorge Pando';
+  num1!: number;
+  num2!: number;
+  resultado!: number;
+
+  public suma() : void {
+    this.resultado = this.num1 + this.num2;
+  }
+
+  public resta() : void {
+    this.resultado = this.num1 - this.num2;
+  }
+
+  public multiplicacion() : void {
+    this.resultado = this.num1 * this.num2;
+  }
+
+  public division() : void {
+    this.resultado = this.num1 / this.num2;
+  }
+
+  public resto() : void {
+    this.resultado = this.num1 % this.num2;
+  }
 }
